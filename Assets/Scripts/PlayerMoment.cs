cb539e0f5d53f0c3eeed6b676c32bb78b6cc8bd6
Sgoel-exe <sgoel@email.arizona.edu>
2023-07-18 15:45:09 -0700

    Asteroids like floaty movement, and screen wrapping

--- Assets/Scripts/PlayerMoment.cs
+++ Assets/Scripts/PlayerMoment.cs
@@ -1,23 +1,86 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.UIElements;
 
 public class PlayerMoment : MonoBehaviour
 {
+    [Header("Inputs")]
     //PlayerComponents
     private Rigidbody2D rb;
-    public 
 
+    public InputAction Movement;
+    public InputAction Rotation;
+
+    private Renderer[] renderers;
+
+    [Header("PlayerMoment")]
+    //Movement related stuff;
+    [SerializeField] private float moveSpeed = 5f;
+    [SerializeField] private float rotationSpeed = 45f;
+
+    //Screen Wrap Stuff
+    [Header("Screen Wrap")]
+    public float screenHeight;
+    public float screenWidth;
+
+
+    private void OnEnable()
+    {
+        Movement.Enable();
+        Rotation.Enable();
+    }
+
+    private void OnDisable()
+    {
+        Movement.Disable();
+        Rotation.Disable();
+    }
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        renderers = GetComponentsInChildren<Renderer>();
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+    }
+
+    private void FixedUpdate()
+    {
+        Move();
+        Rotating();
+        ScreenWrap();
+    }
+
+    void Move()
+    {
+        rb.AddRelativeForce(new Vector2(0, Movement.ReadValue<float>() * moveSpeed), ForceMode2D.Force);
+    }
+
+    void Rotating()
+    {
+        rb.angularVelocity = (Rotation.ReadValue<float>() * rotationSpeed);
+    }
+
+    private void ScreenWrap()
+    {
+        Vector3 pos = transform.position;
+
+        if(Mathf.Abs(pos.y) > screenHeight)
+        {
+            pos.y = -pos.y;
+        }
+
+        if(Mathf.Abs(pos.x) > screenWidth)
+        {
+            pos.x = -pos.x;
+        }
+
+        transform.position = pos;
     }
 }
